572b3c03dff66c4e01224994eb815b1e4afce744
Jakub Mendyk <jakubmendyk.szkola+git@gmail.com>
2018-01-07 17:21:32 +0100

    Create and launch App in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,18 @@
  * @date 05.01.18
  */
 
+#include "app.h"
+
+
+
 int main(int argc, char *argv[]) {
+
+    App* app = App__create();
+
+    if(!app) return 0;
+    App__run(app);
+
+    App__destroy(app);
+
     return 0;
 }
