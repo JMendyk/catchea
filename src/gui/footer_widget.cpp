61c9f48de709e01b025e9644e83e0a725f63b6d7
Jakub Mendyk <jakubmendyk.szkola+git@gmail.com>
2018-01-07 13:43:10 +0100

    Dummy layout for FooterWidget

--- src/gui/footer_widget.cpp
+++ src/gui/footer_widget.cpp
@@ -6,6 +6,8 @@
 
 #include "footer_widget.h"
 
+#include <imgui_internal.h>
+
 
 
 FooterWidget* FooterWidget__create() {
@@ -29,6 +31,49 @@ void FooterWidget__render(FooterWidget* fw, const ImVec2& window_pos, const ImVe
     {
         ImGui::SetWindowPos(window_pos, ImGuiCond_Always);
         ImGui::SetWindowSize(window_size, ImGuiCond_Always);
+
+        for (int idx = 0; idx < 1; idx++) {
+            if (idx == 0) {
+                ImGui::Text("%.1f FPS", ImGui::GetIO().Framerate);
+            }
+            ImGui::SameLine();
+        }
+
+        /*
+         * Solution based on an answer to ImGui issue #934
+         * https://github.com/ocornut/imgui/issues/934#issuecomment-340231002
+         * I (@JMendyk) refactored mentioned solution using a for-loop
+         * thus order of elements is the same as they will appear on the screen
+         */
+
+        static const int elements_count = 4;
+
+        static float widths[elements_count];
+        static const float ItemSpacing = ImGui::GetStyle().ItemSpacing.x;
+        float pos = 0;
+
+        for (int idx = elements_count - 1; idx >= 0; idx--) {
+            pos += widths[idx] + ItemSpacing;
+            ImGui::SameLine(ImGui::GetWindowWidth() - pos);
+            ImGui::BeginGroup();
+            if (idx == 0) {
+                ImGui::Text("Min: %d", -20);
+                ImGui::SameLine();
+                ImGui::VerticalSeparator();
+            } else if (idx == 1) {
+                ImGui::Text("Avg: %d", 67);
+                ImGui::SameLine();
+                ImGui::VerticalSeparator();
+            } else if (idx == 2) {
+                ImGui::Text("Max: %d", 2499);
+                ImGui::SameLine();
+                ImGui::VerticalSeparator();
+            } else if (idx == 3) {
+                ImGui::Text("File: %s", "file1.hgt");
+            }
+            ImGui::EndGroup();
+            widths[idx] = ImGui::GetItemRectSize().x;
+        }
     }
     ImGui::End();
 }
