2e77a7a6f00e3d49321882431a44f309bb22a304
Jakub Mendyk <jakubmendyk.szkola+git@gmail.com>
2018-01-07 17:21:19 +0100

    Add utilities
    Adds util_basename which is a proxy to basename.
    Adds get_time and helper macros for benchmarking performance.

---
+++ src/utils.h
@@ -0,0 +1,24 @@
+/**
+ * @file utils.h
+ * @author JMendyk
+ * @date 06.01.18
+ */
+
+#ifndef CATCHEA_UTILS_H
+#define CATCHEA_UTILS_H
+
+#include <libgen.h>
+
+/**
+ * Get current time in high resolution
+ * @return Current time
+ */
+double get_time();
+
+#define START_BENCH(name) double  bench_start_##name = get_time();
+#define STOP_BENCH(name) double  bench_stop_##name = get_time();
+#define GET_BENCH(name) (bench_stop_##name - bench_start_##name)
+
+char* util_basename(char* path);
+
+#endif //CATCHEA_UTILS_H
